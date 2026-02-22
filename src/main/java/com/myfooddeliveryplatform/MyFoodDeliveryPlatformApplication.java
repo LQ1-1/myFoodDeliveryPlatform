4e9bcaaf980e7ee80f1a904d24c881a7ca1cb175
LQ1-1 <1522788291@qq.com>
2026-02-23 00:14:30 +0800

    empty

---
+++ src/main/java/com/myfooddeliveryplatform/MyFoodDeliveryPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.myfooddeliveryplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFoodDeliveryPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFoodDeliveryPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myfooddeliveryplatform/MyFoodDeliveryPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfooddeliveryplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFoodDeliveryPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
